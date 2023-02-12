a56ad1626b3dfe55aa1245f77b5122aa8bc72d7a
Fulbabu <fulbabucse@gmail.com>
2023-02-12 23:30:28 +0600

    single user get

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,12 +9,24 @@ exports.postUser = async (req, res, next) => {
         $set: {
           name: req.body.name,
           email: req.body.email,
-          photoURL: req.body.photoURL,
+          photoURL: req.body.photo,
         },
       },
       { upsert: true }
     );
     res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+exports.getUser = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.query.email }, { __v: 0 });
+    if (user?.email) {
+      return res.status(200).send({ status: true, user });
+    }
+    res.status(404).send({ status: false, message: "User Not Found" });
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.put("/:email", userController.postUser);
+router.get("/", userController.getUser);
 
 module.exports = router;
